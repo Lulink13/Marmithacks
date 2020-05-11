e4c0ee00b78a036583d05980883ae057cb4f8095
MehdiHamidouche <47760305+MehdiHamidouche@users.noreply.github.com>
2020-05-11 17:27:51 +0200

    nom et image utilisateur + menu avec déconnexion

--- view/header.php
+++ view/header.php
@@ -2,7 +2,17 @@
     <div id="header-titre">Marmit'Hacks</div>
 
     <?php if (isset($_SESSION['user'])) { ?>
-        <div id="header-nom_user"><?php echo $username;?></div>
+        <div class="dropdown" id="header-nom_user">
+            
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <img label=">" src="../assets/images/profile_icon.png"><?php echo $username;?>
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#">Profil</a>
+                <a class="dropdown-item" href="../controller/deconnexion.php">Déconnexion</a>
+                
+            </div>
+        </div>
     <?php } else { ?>
         <a id="header-btn_connexion" href="connexion.php"><img label=">" src="../assets/images/picto_login.png">SE CONNECTER</a>
     <?php } ?>
--- view/homeView.php
+++ view/homeView.php
@@ -4,8 +4,11 @@
         <meta charset="utf-8">
         <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
         <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
+        <script src="../assets/js/jquery.min.js"></script>
+        <script src="../assets/js/popper.min.js"></script>
     </head>
     <body>
         <?php require_once 'header.php';?>
+        <script src="../assets/js/bootstrap.min.js"></script>
     </body>
 </html>
