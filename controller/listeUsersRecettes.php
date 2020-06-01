9e8f5b4056b7f1e17c56f187baa3e8cdac9c6499
MehdiHamidouche <47760305+MehdiHamidouche@users.noreply.github.com>
2020-06-01 16:54:16 +0200

    gestion des recettes par un utilisateur
    ajout de la gestion des recettes par un utilisateur. A FAIRE : modification et suppression des recettes + mise en page (CSS)

---
+++ controller/listeUsersRecettes.php
@@ -0,0 +1,12 @@
+<?php
+
+$listUsersRecipes = $bdd->getListUsersRecipes($validate,$idUser);
+foreach ($listUsersRecipes as $actualUsersRecipe) {
+    if ($actualUsersRecipe->getPicture() != '' && file_exists('../data/recipe_pictures/'.$actualUsersRecipe->getPicture())) {
+        $recipePicture = '../data/profile_pictures/'.$actualUsersRecipe->getPicture();
+    } else {
+        $recipePicture = '../assets/images/recipe_default.jpg';
+    }
+    include '../view/vignetteUserRecetteView.php';
+}
+?>
